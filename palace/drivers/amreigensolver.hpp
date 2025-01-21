3cb77eb122201c57439d25f26242c560fc7ca9ac
Adhish Chakravorty <skjetly094@gmail.com>
2025-01-21 12:04:21 -0800

    Update amreigensolver.hpp

--- palace/drivers/amreigensolver.hpp
+++ palace/drivers/amreigensolver.hpp
@@ -16,7 +16,17 @@ class ErrorIndicator;
 
 class AMREigenSolver : public BaseSolver
 {
+private:
+  IoData &iodata;
+  bool root;
+  std::size_t size;
+  int num_thread;
+  std::string git_tag;
+
 public:
+  AMREigenSolver(IoData &iodata, bool root, std::size_t size, int num_thread,
+                 const std::string &git_tag)
+      : iodata(iodata), root(root), size(size), num_thread(num_thread), git_tag(git_tag) {}
 // This driver does repeated solves with local refinement near the junction
 // Returns the final (ErrorIndicator, vsize) from the last solve
   std::pair<ErrorIndicator, long long int>
